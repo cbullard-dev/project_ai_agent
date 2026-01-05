f7c4e4be7d92ee8cfca59d760d84f6ebe1b5b34b
Cal <60569006+cbullard-dev@users.noreply.github.com>
2026-01-05 15:51:36 +0900

    Updated verbose vs normal outputs

--- main.py
+++ main.py
@@ -2,32 +2,44 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     parser = argparse.ArgumentParser(description="AI Chatbot")
     parser.add_argument("user_prompt",type=str,help="User prompt")
+    parser.add_argument("--verbose",action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     AI_MODEL = "gemini-2.5-flash"
     prompt_data = args.user_prompt
+    messages = [types.Content(role="user",parts=[types.Part(text=prompt_data)])]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model= AI_MODEL,contents=prompt_data)
+    response = client.models.generate_content(model= AI_MODEL,contents=messages)
     usage_metadata = response.usage_metadata
     if usage_metadata == None:
         raise Exception("response metadata not present")
     response_text = response.text
     prompt_tokens = usage_metadata.prompt_token_count
     response_tokens = usage_metadata.candidates_token_count
-    print_format = f"""
+    verbose_print_format = f"""
         User prompt: {prompt_data}
         Prompt tokens: {prompt_tokens}
         Response tokens: {response_tokens}
         Response:
         {response_text}
     """
+
+    print_format = f"""
+    Response:
+    {response_text}
+    """
+
+    if args.verbose:
+        print_format = verbose_print_format
+
     print(print_format)
 
 
